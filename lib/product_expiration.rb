edb23b9f773860d86fb84b08aff4d1e971beea75
Michael Guterl <michael@diminishing.org>
2011-09-23 09:12:09 -0400

    specify table name in conditions

--- lib/product_expiration.rb
+++ lib/product_expiration.rb
@@ -1,6 +1,9 @@
 module ProductExpiration
   def self.included(base)
-    base.named_scope :not_deleted, lambda { { :conditions => ["(deleted_at IS NULL OR deleted_at > ?)", Time.now.utc.to_s(:db)] } }
+    table_name = base.table_name
+    base.named_scope :not_deleted, lambda { 
+      { :conditions => ["(#{table_name}.deleted_at IS NULL OR #{table_name}.deleted_at > ?)", Time.now.utc.to_s(:db)] } 
+    }
     base.class_eval do
       def deleted?
         deleted_at && deleted_at <= Time.now
